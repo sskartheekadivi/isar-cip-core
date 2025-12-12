sskartheekadivi/isar-cip-core

commit 4ba393e85e5a832e4248b6842badec41207846cd

u-boot: Set COMPATIBLE_MACHINE to drop PREFERRED_PROVIDER_u-boot-*
This is better to maintain as it keeps the key information local to the
u-boot recipe.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

recipes-bsp/u-boot/u-boot-bbb_2025.07.bb
@@ -1,7 +1,7 @@
 #
 # CIP Core, generic profile
 #
-# Copyright (c) Siemens AG, 2022-2024
+# Copyright (c) Siemens AG, 2022-2025
 #
 # Authors:
 #  Jan Kiszka <jan.kiszka@siemens.com>
@@ -10,6 +10,8 @@
 #
 
 require u-boot-common-${PV}.inc
+
+COMPATIBLE_MACHINE = "bbb"
 
 U_BOOT_CONFIG = "am335x_evm_defconfig"
 U_BOOT_BIN = "all"

recipes-bsp/u-boot/u-boot-qemu-arm64_2025.07.bb
@@ -1,7 +1,7 @@
 #
 # CIP Core, generic profile
 #
-# Copyright (c) Siemens AG, 2022
+# Copyright (c) Siemens AG, 2022-2025
 #
 # Authors:
 #  Jan Kiszka <jan.kiszka@siemens.com>
@@ -11,5 +11,7 @@
 
 require u-boot-common-${PV}.inc
 require u-boot-qemu-common.inc
+
+COMPATIBLE_MACHINE = "qemu-arm64"
 
 U_BOOT_CONFIG = "qemu_arm64_defconfig"

recipes-bsp/u-boot/u-boot-qemu-arm_2025.07.bb
@@ -1,7 +1,7 @@
 #
 # CIP Core, generic profile
 #
-# Copyright (c) Siemens AG, 2022
+# Copyright (c) Siemens AG, 2022-2025
 #
 # Authors:
 #  Jan Kiszka <jan.kiszka@siemens.com>
@@ -11,5 +11,7 @@
 
 require u-boot-common-${PV}.inc
 require u-boot-qemu-common.inc
+
+COMPATIBLE_MACHINE = "qemu-arm"
 
 U_BOOT_CONFIG = "qemu_arm_defconfig"

recipes-bsp/u-boot/u-boot-qemu-riscv64_2025.07.bb
@@ -1,7 +1,7 @@
 #
 # CIP Core, generic profile
 #
-# Copyright (c) Siemens AG, 2022-2024
+# Copyright (c) Siemens AG, 2022-2025
 #
 # Authors:
 #  Jan Kiszka <jan.kiszka@siemens.com>
@@ -11,6 +11,8 @@
 
 require u-boot-common-${PV}.inc
 require u-boot-qemu-common.inc
+
+COMPATIBLE_MACHINE = "qemu-riscv64"
 
 # we run as OpenSBI payload, hence use smode
 U_BOOT_CONFIG = "${MACHINE}_smode_defconfig"

conf/machine/bbb.conf
@@ -19,5 +19,4 @@ KERNEL_DEFCONFIG = "cip-kernel-config/${KERNEL_DEFCONFIG_VERSION}/arm/cip_bbb_de
 # for SWUpdate setups: watchdog is configured in U-Boot
 WDOG_TIMEOUT = "0"
 
-PREFERRED_PROVIDER_u-boot-${MACHINE} = "u-boot-bbb"
 PREFERRED_PROVIDER_factory-reset-helper = "factory-reset-helper-file"

conf/machine/qemu-arm.conf
@@ -16,6 +16,4 @@ KERNEL_DEFCONFIG ?= "cip-kernel-config/${KERNEL_DEFCONFIG_VERSION}/arm/qemu_arm_
 # watchdog is not yet supported in our QEMU executor for this platform, disable it
 WDOG_TIMEOUT = "0"
 
-PREFERRED_PROVIDER_u-boot-${MACHINE} = "u-boot-qemu-arm"
-
 PREFERRED_PROVIDER_factory-reset-helper = "factory-reset-helper-file"

conf/machine/qemu-arm64.conf
@@ -17,6 +17,4 @@ KERNEL_DEFCONFIG ?= "cip-kernel-config/${KERNEL_DEFCONFIG_VERSION}/arm64/cip_mer
 # watchdog is not yet supported in our QEMU executor for this platform, disable it
 WDOG_TIMEOUT = "0"
 
-PREFERRED_PROVIDER_u-boot-${MACHINE} = "u-boot-qemu-arm64"
-
 PREFERRED_PROVIDER_factory-reset-helper = "factory-reset-helper-file"

conf/machine/qemu-riscv64.conf
@@ -19,6 +19,4 @@ KERNEL_DEFCONFIG ?= "cip-kernel-config/${KERNEL_DEFCONFIG_VERSION}/riscv/cip_mer
 # watchdog is not yet supported in our QEMU executor for this platform, disable it
 WDOG_TIMEOUT = "0"
 
-PREFERRED_PROVIDER_u-boot-${MACHINE} = "u-boot-qemu-riscv64"
-
 PREFERRED_PROVIDER_factory-reset-helper = "factory-reset-helper-file"

commit 10731dde563b0a9d0a2b6ba4458c13d1805378c5

Use exact matches for COMPATIBLE_MACHINE
COMPATIBLE_MACHINE is evaluated as regular expression. To avoid wrong
matches, e.g. qemu-arm on qemu-arm64, make sure that we only match if
the current machine is identical to the expected one.

Brackets are not strictly needed for the match but improve readability.

Fixes: 4ba393e85e5a ("u-boot: Set COMPATIBLE_MACHINE to drop PREFERRED_PROVIDER_u-boot-*")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

recipes-bsp/trusted-firmware-a/trusted-firmware-a-ti-k3_2.13.bb
@@ -19,4 +19,4 @@ TF_A_PLATFORM = "k3"
 TF_A_EXTRA_BUILDARGS = "CFG_ARM64=y TARGET_BOARD=lite SPD=opteed K3_PM_SYSTEM_SUSPEND=1"
 TF_A_BINARIES = "lite/release/bl31.bin"
 
-COMPATIBLE_MACHINE = "ti-am62px-sk"
+COMPATIBLE_MACHINE = "^(ti-am62px-sk)$"

recipes-bsp/u-boot/u-boot-bbb_2025.07.bb
@@ -11,7 +11,7 @@
 
 require u-boot-common-${PV}.inc
 
-COMPATIBLE_MACHINE = "bbb"
+COMPATIBLE_MACHINE = "^(bbb)$"
 
 U_BOOT_CONFIG = "am335x_evm_defconfig"
 U_BOOT_BIN = "all"

recipes-bsp/u-boot/u-boot-de0-nano-soc_2025.07.bb
@@ -11,7 +11,7 @@
 
 require u-boot-common-${PV}.inc
 
-COMPATIBLE_MACHINE = "de0-nano-soc"
+COMPATIBLE_MACHINE = "^(de0-nano-soc)$"
 
 U_BOOT_CONFIG = "socfpga_de0_nano_soc_defconfig"
 U_BOOT_BIN = "u-boot-with-spl.sfp"

recipes-bsp/u-boot/u-boot-qemu-arm64_2025.07.bb
@@ -12,6 +12,6 @@
 require u-boot-common-${PV}.inc
 require u-boot-qemu-common.inc
 
-COMPATIBLE_MACHINE = "qemu-arm64"
+COMPATIBLE_MACHINE = "^(qemu-arm64)$"
 
 U_BOOT_CONFIG = "qemu_arm64_defconfig"

recipes-bsp/u-boot/u-boot-qemu-arm_2025.07.bb
@@ -12,6 +12,6 @@
 require u-boot-common-${PV}.inc
 require u-boot-qemu-common.inc
 
-COMPATIBLE_MACHINE = "qemu-arm"
+COMPATIBLE_MACHINE = "^(qemu-arm)$"
 
 U_BOOT_CONFIG = "qemu_arm_defconfig"

recipes-bsp/u-boot/u-boot-qemu-riscv64_2025.07.bb
@@ -12,7 +12,7 @@
 require u-boot-common-${PV}.inc
 require u-boot-qemu-common.inc
 
-COMPATIBLE_MACHINE = "qemu-riscv64"
+COMPATIBLE_MACHINE = "^(qemu-riscv64)$"
 
 # we run as OpenSBI payload, hence use smode
 U_BOOT_CONFIG = "${MACHINE}_smode_defconfig"

recipes-bsp/u-boot/u-boot-ti-am62px-sk_2025.07.bb
@@ -35,7 +35,7 @@ SRC_URI[am62p-sysfw-enc-hs.sha256sum] = "4d0ed21a36dd57ada78307506403204e179d81d
 S = "${WORKDIR}/u-boot-${PV}"
 TI_LINUX_FIRMWARE = "${S}/ti-linux-firmware"
 
-COMPATIBLE_MACHINE = "ti-am62px-sk"
+COMPATIBLE_MACHINE = "^(ti-am62px-sk)$"
 
 U_BOOT_R5_CONFIG = "am62px_evm_r5_defconfig"
 U_BOOT_A53_CONFIG = "am62px_evm_a53_defconfig"